5efa6c2fa46292ab0a09a9961d467829482c8033
Oluka Isaac <hilghsilk2damson@gmail.com>
2022-08-06 00:37:43 +0100

    initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# React-card-slider
+A responsive and easy to use react-card-slider.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React-slider</h1>
+    </div>
+  );
+}
+
+export default App;
